96e74df872cba32d4e1b92c75ae7b273e9fd927c
Noé Plantier <plantiernoe50@gmail.com>
2024-08-14 16:01:13 +0200

    fix: Header/Footer modifs

--- bordeaux-jet/src/components/NavigationMenu.tsx
+++ bordeaux-jet/src/components/NavigationMenu.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Menu, MenuItem, Button, ListItemText } from '@mui/material';
-import { useNavigate } from 'react-router-dom'; // Importation du hook pour la navigation
+import { MenuItem, Button, ListItemText } from '@mui/material';
+import { useNavigate } from 'react-router-dom'; 
 import '../styles/NavigationMenu.scss';
 
 function NavigationMenu() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const navigate = useNavigate(); // Utilisation du hook useNavigate pour la navigation
+  const navigate = useNavigate();
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -14,27 +14,13 @@ function NavigationMenu() {
   const handleClose = (path: string) => {
     setAnchorEl(null);
     if (path) {
-      navigate(path); // Navigation vers la page correspondante
+      navigate(path); 
     }
   };
 
   return (
     <nav className="navigation-menu">
-      <Button
-        aria-controls="simple-menu"
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
-        Menu
-      </Button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={() => handleClose('')}
-        className="menu-horizontal" // Ajout d'une classe CSS pour le style horizontal
-      >
+   
         <MenuItem onClick={() => handleClose('/')}>
           <ListItemText primary="Accueil" />
         </MenuItem>
@@ -53,7 +39,7 @@ function NavigationMenu() {
         <MenuItem onClick={() => handleClose('/faq')}>
           <ListItemText primary="FAQ" />
         </MenuItem>
-      </Menu>
+      
     </nav>
   );
 }
