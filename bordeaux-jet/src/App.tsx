b68a3bdfe7290cd4b338a3ecf5d391bcc6794eb2
Noé Plantier <plantiernoe50@gmail.com>
2024-08-14 15:40:17 +0200

    fix: Routes settings

--- bordeaux-jet/src/App.tsx
+++ bordeaux-jet/src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Formules from './components/Formules';
+import Contact from './components/ Contact';
+import Galerie from './components/ Gallery';
+import FAQ from './components/FAQ';
 import Reservation from './components/Reservation';
+import NavigationMenu from './components/NavigationMenu';
 
 function App() {
   return (
     <Router>
+  
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/formules" element={<Formules />} />
+   
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/galerie" element={<Galerie />} />
+        <Route path="/faq" element={<FAQ />} />
         <Route path="/reservation" element={<Reservation />} />
       </Routes>
     </Router>
--- bordeaux-jet/src/components/NavigationMenu.tsx
+++ bordeaux-jet/src/components/NavigationMenu.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Menu, MenuItem, Button, ListItemText } from '@mui/material';
+import { useNavigate } from 'react-router-dom'; // Importation du hook pour la navigation
 import '../styles/NavigationMenu.scss';
 
 function NavigationMenu() {
-
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  
+  const navigate = useNavigate(); // Utilisation du hook useNavigate pour la navigation
+
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleClose = () => {
+  const handleClose = (path: string) => {
     setAnchorEl(null);
+    if (path) {
+      navigate(path); // Navigation vers la page correspondante
+    }
   };
 
   return (
@@ -28,29 +32,30 @@ function NavigationMenu() {
         anchorEl={anchorEl}
         keepMounted
         open={Boolean(anchorEl)}
-        onClose={handleClose}
+        onClose={() => handleClose('')}
+        className="menu-horizontal" // Ajout d'une classe CSS pour le style horizontal
       >
-        <MenuItem onClick={handleClose}>
+        <MenuItem onClick={() => handleClose('/')}>
           <ListItemText primary="Accueil" />
         </MenuItem>
-        <MenuItem onClick={handleClose}>
+        <MenuItem onClick={() => handleClose('/formules')}>
           <ListItemText primary="Formules" />
         </MenuItem>
-        <MenuItem onClick={handleClose}>
+        <MenuItem onClick={() => handleClose('/acces')}>
           <ListItemText primary="Accès" />
         </MenuItem>
-        <MenuItem onClick={handleClose}>
+        <MenuItem onClick={() => handleClose('/contact')}>
           <ListItemText primary="Contact" />
         </MenuItem>
-        <MenuItem onClick={handleClose}>
+        <MenuItem onClick={() => handleClose('/galerie')}>
           <ListItemText primary="Galerie" />
         </MenuItem>
-        <MenuItem onClick={handleClose}>
+        <MenuItem onClick={() => handleClose('/faq')}>
           <ListItemText primary="FAQ" />
         </MenuItem>
       </Menu>
     </nav>
   );
-};
+}
 
 export default NavigationMenu;
